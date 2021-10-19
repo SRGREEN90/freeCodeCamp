a7c2199aa1b32bd451fed6208c0fea403feeec10
James Brown <jmsbrown63@gmail.com>
2021-10-19 23:29:52 +0300

    addFive

---
+++ codeWars/cw2.js
@@ -0,0 +1,12 @@
+function addFive(num) {
+    var total = num + 5
+    return total
+  }
+
+  describe('fix add five', () => {
+    it('fixed tests', () => {
+      Test.assertEquals(addFive(5), 10)
+      Test.assertEquals(addFive(0), 5)
+      Test.assertEquals(addFive(-5), 0)
+    })
+  })

922292d63e23c046ed5cbfba25b00c3e6635ab48
James Brown <jmsbrown63@gmail.com>
2021-10-24 11:01:06 +0300

    moveZeros

--- codeWars/cw23.js
+++ codeWars/cw23.js
@@ -2,4 +2,5 @@ var moveZeros = function (arr) {
     const zeros = arr.filter (arr => arr === 0);
        const others = arr.filter (arr => arr !== 0);
        return others.concat(zeros);   
+       
    }

e00ce06df65460cc554739d9d025076e1045a678
SRGREEN90 <you@example.com>
2021-11-29 10:23:30 +0300

    myDog

---
+++ freeCodeCamp/78.js
@@ -0,0 +1,8 @@
+const myDog = {
+    // Only change code below this line
+    name: 'Sharik',
+    legs: 4,
+    tails: 1,
+    friends: ['cat', 'dog', 'mouse']
+    // Only change code above this line
+};

8a5a39dbb29a153243c83b356eb66c739601cc8f
SRGREEN90 <you@example.com>
2021-12-02 19:00:29 +0300

    booleanToString

--- freeCodeCamp/79.js
+++ freeCodeCamp/79.js
@@ -4,7 +4,6 @@ const testObj = {
     "shirt": "jersey",
     "shoes": "cleats"
 };
-
 // Only change code below this line
 const hatValue = testObj.hat;      // Change this line
 const shirtValue = testObj.shirt;    // Change this line

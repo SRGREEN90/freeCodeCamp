0eb1b2ad54b1d5732bc00c46bebc588f1f5725fe
SRGREEN90 <you@example.com>
2022-07-23 22:54:21 +0300

    humanYearsCatYearsDogYears

--- codeWars/cw65.js
+++ codeWars/cw65.js
@@ -1,7 +1,20 @@
-function iceBrickVolume(radius, bottleLength, rimLength) {
-    let diaganal = radius * 2
-    let sideSquare = diaganal / Math.sqrt(2)
-    let brickLength = bottleLength - rimLength
-    let result = sideSquare * sideSquare * brickLength
-    return Math.ceil(result)
+const humanYearsCatYearsDogYears = function (humanYears) {
+    let catYears = 0
+    if (humanYears === 1) {
+        catYears = 15
+    } else if (humanYears === 2) {
+        catYears = 15 + 9
+    } else if (humanYears > 2) {
+        catYears = 15 + 9 + (4 * (humanYears - 2))
+    }
+    let dogYears = 0
+    if (humanYears === 1) {
+        dogYears = 15
+    } else if (humanYears === 2) {
+        dogYears = 15 + 9
+    } else if (humanYears > 2) {
+        dogYears = 15 + 9 + (5 * (humanYears - 2))
+    }
+    return [humanYears, catYears, dogYears]
 }
+
